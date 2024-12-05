7eba483e2de71106a1d607cb02bdce766a7e2a9a
Miguel Prieto <miguel.prieto@orkes.io>
2024-12-04 21:01:19 -0300

    C# worker examples for the developer-guides

---
+++ csharp/developer-guides/using-workers/Worker.cs
@@ -0,0 +1,53 @@
+﻿using Conductor.Client.Authentication;
+using Conductor.Client.Worker;
+using Conductor.Client.Models;
+using Conductor.Client.Extensions;
+using Conductor.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace example
+{
+    [WorkerTask]
+    public class MyWorker
+    {
+        [WorkerTask(taskType: "myTask", batchSize: 5, pollIntervalMs: 500, workerId: "sample-csharp-worker")]
+        public static TaskResult MyTask(Conductor.Client.Models.Task task)
+        {
+            var inputData = task.InputData;
+            var result = task.ToTaskResult();
+            result.OutputData = new Dictionary<string, object>
+            {
+                ["message"] = "Hello " + inputData.GetValueOrDefault("name", null)
+            };
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            ApiExtensions.Configuration = new Configuration
+            {
+                BasePath = "https://developer.orkescloud.com/api",
+                AuthenticationSettings = new OrkesAuthenticationSettings("_CHANGE_ME_", "_CHANGE_ME_")
+            };
+
+            var host = WorkflowTaskHost.CreateWorkerHost(
+                ApiExtensions.Configuration,
+                LogLevel.Information
+            );
+            host.Start();
+
+            Console.WriteLine("Press Ctrl+C to exit.");
+            var evt = new ManualResetEvent(false);
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                Console.WriteLine("Ctrl+C pressed. Shutting down...");
+                evt.Set();
+            };
+
+            evt.WaitOne();
+            host.StopAsync();
+        }
+    }
+}
